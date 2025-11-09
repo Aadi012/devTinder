14aab54e4394c8cc87ceb428412b9665adab0327
Aadi012 <jhaaditya778@gmail.com>
2025-11-10 01:00:37 +0530

    exploring GET, POST,DELETE method.

--- src/app.js
+++ src/app.js
@@ -2,18 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.use("/",(req,res,next) =>{
-     res.send("Namaste Aditya,how are you?");
+app.get("/user",(req,res)=>{
+     res.send({firstName:"Aditya", lastName:"Jha"});
 });
 
-app.use("/test",(req,res) =>{
-     res.send("server is listening from test");
+app.post("/user",(req,res)=>{
+     res.send("data saved sucessfully in database...!")
+})
+ 
+app.delete("/user",(req,res)=>{
+     res.send("user is deleted now.!")
+})
+app.use("/test", (req, res) => {
+  res.send("server is listening from test");
 });
 
-app.use("/hello",(req,res) =>{
-     res.send("server is saying hello");
-});
-
-app.listen(8888,()=>{
-    console.log("server is working on 8888...");
+app.listen(8888, () => {
+  console.log("server is working on 8888...");
 });
