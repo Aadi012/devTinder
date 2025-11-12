9b39a939bdfd3e1115d13b4d97f021204084c794
Aadi012 <jhaaditya778@gmail.com>
2025-11-12 23:33:25 +0530

    GET APIs

--- src/app.js
+++ src/app.js
@@ -8,13 +8,41 @@ app.use(express.json());
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
 
-  
+
   try {
     await user.save();
     res.send("User Added Successfully...!!");
   } catch (err) {
     res.status(400).send("Error saving the user:" + err.message);
   }
+});
+
+// get a user by email
+app.get("/user", async (req, res) =>{
+     const userEmail = req.body.emailId;
+     try {
+        const users = await User.find({emailId : userEmail});
+        if(users.length===0){
+          res.status(404).send("user not found");
+        }else{
+            res.send(users);
+        }
+        
+     }
+    catch (err) {
+      res.status(400).send("Something went wrong");
+    }
+});
+
+// Feed API - Get /feed - get all the user from database
+
+app.get("/feed", async (req, res) =>{
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }catch (err) {
+      res.status(400).send("Something went wrong");
+    }
 });
 
 connectDB()
