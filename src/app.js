a2f78ce17ff8eef7fe3a4bc23a563e404e069697
Aadi012 <jhaaditya778@gmail.com>
2025-11-13 22:14:20 +0530

    API Level Data Sanitization

--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ app.use(express.json());
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
 
-
   try {
     await user.save();
     res.send("User Added Successfully...!!");
@@ -18,56 +17,73 @@ app.post("/signup", async (req, res) => {
 });
 
 // get a user by email
-app.get("/user", async (req, res) =>{
-     const userEmail = req.body.emailId;
-     try {
-        const users = await User.find({emailId : userEmail});
-        if(users.length===0){
-          res.status(404).send("user not found");
-        }else{
-            res.send(users);
-        }
-        
-     }
-    catch (err) {
-      res.status(400).send("Something went wrong");
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("user not found");
+    } else {
+      res.send(users);
     }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
 });
 
 // Feed API - Get /feed - get all the user from database
 
-app.get("/feed", async (req, res) =>{
-    try{
-        const users = await User.find({});
-        res.send(users);
-    }catch (err) {
-      res.status(400).send("Something went wrong");
-    }
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
 });
 // delete a user from database
-app.delete("/user", async (req, res) =>{
-    const userId = req.body.userId;
-   try{
-     const user = await User.findByIdAndDelete({_id:userId});
-     res.send("user deleted successfully..!");
-   }catch (err) {
-      res.status(400).send("Something went wrong");
-    }
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete({ _id: userId });
+    res.send("user deleted successfully..!");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
 });
 
-app.patch("/user", async (req, res) =>{
-    const userId = req.body.userId;
-    const data = req.body;
-    try {
-       const user = await User.findByIdAndUpdate({_id:userId}, data,{
-         returnDocument:"after",
-         runValidators:true,
-       });
-       res.send("user updated successfully");
-    }catch (err) {
-      res.status(400).send("Something went wrong");
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+
+  try {
+    const allowedUpdates = ["photoUrl", "skills", "age", "gender", "about"];
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      allowedUpdates.includes(k)
+    );
+
+    if (!isUpdateAllowed) {
+       res.status(400).send("Updates are not allowed");
     }
+
+    if (Array.isArray(data.skills) && data.skills.length > 10) {
+        res.status(400).send("You cannot add more than 10 skills");
+    }
+
+    const user = await User.findByIdAndUpdate(userId, data, {
+      returnDocument: 'after',
+      runValidators: true,
+    });
+
+    if (!user) res.status(404).send("User not found");
+
+    res.status(200).json({ message: "User updated successfully", user });
+  } catch (err) {
+    console.error(err);
+    res.status(400).send("Something went wrong");
+  }
 });
+
 
 connectDB()
   .then(() => {
