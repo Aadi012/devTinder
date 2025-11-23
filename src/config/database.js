8e536bb0fcb5a8a3a1566ae673a16de9381dd3b1
Aadi012 <jhaaditya778@gmail.com>
2025-11-23 18:28:23 +0530

    fixed cors error

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://jhaaditya778_db_user:IC7dsTQruaLUI4qn@backendproject.msuidnk.mongodb.net/devTinder"
-  );
+  try {
+    await mongoose.connect(
+      "mongodb+srv://jhaaditya778_db_user:IC7dsTQruaLUI4qn@backendproject.msuidnk.mongodb.net/devTinder"
+    );
+    console.log("Database connected successfully!");
+  } catch (error) {
+    console.error("Database cannot be connected!!", error.message);
+    // process.exit(1);
+  }
 };
- 
+
 module.exports = connectDB;
